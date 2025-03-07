87b832b7352511b78c5c11e48a3005fd3095c967
Dursun Köktürk <dursunserefkokturk@gmail.com>
2025-03-07 18:40:56 +0300

    clerk-making

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface AuthLayoutProps{
+    children:React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+        <div className='flex flex-col item-center'>
+            { children }
+        </div>
+    </div>
+  )
+}
+
+export default AuthLayout
